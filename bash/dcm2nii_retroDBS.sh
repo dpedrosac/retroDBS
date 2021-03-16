f4fa16072e867528eafcd08d1efa820aaf0db67a
david <46906261+dpedrosac@users.noreply.github.com>
2021-03-16 17:01:41 +0100

    Update dcm2nii_retroDBS.sh
    First draft of a preprocessing pipeline for imaging data

--- bash/dcm2nii_retroDBS.sh
+++ bash/dcm2nii_retroDBS.sh
@@ -1,13 +1,56 @@
 #!/bin/bash
-# Purpose: Read Comma Separated CSV File
+# Purpose: Read Comma Separated CSV File and convert DICOM to nifti with
+# Chris Rorden's software (dcm2niix, cf. https://github.com/rordenlab/dcm2niix)
 # Author: Kenan Steidel, Philipp Loehrer and David Pedrosa under MIT
 # -----------------------------------------------------------------
 
-export PATH2DCM2NIIX= #~/usr/local/fsl/
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+
+
+# check for dcm2nii existence at default location/opt/bin (is that right??)
+if ! command -v COMMAND &> /dev/null #add dcm2nii
+then
+    echo "COMMAND could not be found, building dcm2nii from scratch to default folder"
+    TMP_DIR=tmp$RANDOM
+    mkdir -p $TMP_DIR    
+    
+    cd TMP_DIR
+    git clone https://github.com/rordenlab/dcm2niix.git
+    cd dcm2niix
+    mkdir build && cd build
+    cmake ..
+    make
+    rm -rf TMP_DIR
+fi
+
+export PATH2DCM2NIIX= #~/usr/local/??/
 export PATH=${PATH2DCM2NIIX}:$PATH
 
 # Read csv-file to get the necessary information
-INPUT=iPS-DBS.csv
+INPUT=../../iPS-DBS.csv
+INPUT_DIR=abc
+OUTPUT_DIR=../../convertedImaging/
+if [[ ! -f INPUT_DIR ]] ; then
+    echo 'Input directory is not defined, aborting the script.'
+    exit
+fi
+
+# Creates virtual environment if not present, activates it and tries to install packages needed 
+venv_DIR=../../venv/
+if [[ ! -f venv_DIR ]] ; then
+    echo 'creating virtual environment for defacing.'
+    python3 -m venv ../../venv
+    source ../../venv/bin/activate
+    pip install pydeface
+fi
+
 
 imagingModality=( CT MRI )
 OLDIFS=$IFS
@@ -22,29 +65,11 @@ do
   for i in "${imagingModality[@]}"
   do  
     echo "======== converting $i imaging"
-    dcm2nii
+    PATH2DCM2NIIX/dcm2niix.sh 
+    -ba y \
+    -f $Pseud_$i%s%d.nii.g\
+    -i $INPUT_DIR/$Surname_$Name$i/ \
+    -o OUTPUT_DIR
   done
 done < $INPUT
 IFS=$OLDIFS
-
-
-
-# Convert images.
-cmd="$exenam $endian -b y -z n -f %p_%s -o $outdir $indir"
-
-echo "Running command:"
-echo $cmd
-
-$cmd
-
-
-# Validate JSON.
-exists python &&
-    {
-        printf "\n\n\nValidating JSON files.\n\n\n"
-        for file in $outdir/*.json; do
-            echo -n "$file "
-            ! python -m json.tool "$file" > /dev/null || echo " --  Valid."
-        done
-        printf "\n\n\n"
-    }
