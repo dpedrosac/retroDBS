0f99dc0160921eafc19da0e26b9ca6373b308df9
dpedrosac <david.pedrosa@staff.uni-marburg.de>
2021-03-17 00:29:43 +0100

    changes in the way dcm2niix is called and the way data is preprocessed

--- bash/dcm2nii_retroDBS.sh
+++ bash/dcm2nii_retroDBS.sh
@@ -4,50 +4,77 @@
 # Author: Kenan Steidel, Philipp Loehrer and David Pedrosa under MIT
 # -----------------------------------------------------------------
 
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+CURRENT_DIR=${PWD}
+METADATA=${PWD}/metadata/iPS-DBS.csv
 
-
-
-# check for dcm2nii existence at default location/opt/bin (is that right??)
-if ! command -v COMMAND &> /dev/null #add dcm2nii
-then
-    echo "COMMAND could not be found, building dcm2nii from scratch to default folder"
-    TMP_DIR=tmp$RANDOM
-    mkdir -p $TMP_DIR    
-    
-    cd TMP_DIR
-    git clone https://github.com/rordenlab/dcm2niix.git
-    cd dcm2niix
-    mkdir build && cd build
-    cmake ..
-    make
-    rm -rf TMP_DIR
+if [[ ! -f $METADATA ]] ; then
+	echo
+	echo "--------------------------------------------------------------------------------------"
+	echo " Metadata unavailable. Make sure that csv-file is present "
+	echo "--------------------------------------------------------------------------------------"
+	echo
+	exit
 fi
 
-export PATH2DCM2NIIX= #~/usr/local/??/
+INSTALL_DIR=/opt/dcm2niix # directory at which dcm2niix will be built
+export PATH2DCM2NIIX=${INSTALL_DIR}/build/bin/
 export PATH=${PATH2DCM2NIIX}:$PATH
 
-# Read csv-file to get the necessary information
-INPUT=../../iPS-DBS.csv
-INPUT_DIR=abc
-OUTPUT_DIR=../../convertedImaging/
-if [[ ! -f INPUT_DIR ]] ; then
-    echo 'Input directory is not defined, aborting the script.'
-    exit
+echo
+echo "--------------------------------------------------------------------------------------"
+echo " Checking 'dcm2niix' existence to process DICOMs... "
+
+if ! command -v dcm2niix &> /dev/null #add dcm2nii
+then
+	echo "			dcm2niix is not installed in the default directory, building it from source"
+	echo "--------------------------------------------------------------------------------------"
+	echo
+
+    sudo rm -rf ${INSTALL_DIR}
+	echo "dcm2niix could not be found, building it from scratch to default folder /opt/dcm2niix/"
+    cd /opt/
+	sudo git clone https://github.com/rordenlab/dcm2niix.git
+	sudo chmod 755 -R ${INSTALL_DIR}/*
+
+	cd dcm2niix
+    sudo mkdir build && cd build
+    sudo cmake ..
+    sudo make
+	cd ${DIR}
+else
+	echo "			dcm2niix is built and can be called. Proceeding!"
+	echo "--------------------------------------------------------------------------------------"
+	echo
 fi
 
+# Read csv-file to get the necessary information
+INPUT_DIR=${CURRENT_DIR}/testdata
+if [[ ! -f ${INPUT_DIR} ]] ; then
+	echo
+	echo "--------------------------------------------------------------------------------------"
+	echo " Directory of DICOM folders not defined. Aborting the script "
+	echo "--------------------------------------------------------------------------------------"
+	echo
+    # exit
+fi
+
+OUTPUT_DIR=${CURRENT_DIR}/convertedImaging
+if [[ ! -d $OUTPUT_DIR ]];
+  then
+    mkdir -p $OUTPUT_DIR
+  fi
+
 # Creates virtual environment if not present, activates it and tries to install packages needed 
-venv_DIR=../../venv/
-if [[ ! -f venv_DIR ]] ; then
-    echo 'creating virtual environment for defacing.'
-    python3 -m venv ../../venv
-    source ../../venv/bin/activate
+venv_DIR=${CURRENT_DIR}/venv
+if [[ ! -d ${venv_DIR} ]] ; then
+	echo
+	echo "--------------------------------------------------------------------------------------"
+	echo " Virtual environment is needed. Processing with activation"
+	echo "--------------------------------------------------------------------------------------"
+	echo
+
+    python3 -m venv ${venv_DIR}
+    source venv ${venv_DIR}/venv/bin/activate
     pip install pydeface
 fi
 
@@ -55,21 +82,44 @@ fi
 imagingModality=( CT MRI )
 OLDIFS=$IFS
 IFS=','
-[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-while read Surname Name Pseud
+[ ! -f $METADATA ] && { echo "$METADATA file not found"; exit 99; }
+while read Surname Name pseud rest
 do
-	echo "converting Surname : $Surname, $Name and saving as $Pseud" 
-	# echo "Name : $Name"
-	# echo "Pseudonym : $Pseud"
-
+	echo
+	echo "--------------------------------------------------------------------------------------"
+	echo "Converting: $Surname, $Name to nifti as $pseud" 
+	echo 
+	
   for i in "${imagingModality[@]}"
-  do  
-    echo "======== converting $i imaging"
-    PATH2DCM2NIIX/dcm2niix.sh 
-    -ba y \
-    -f $Pseud_$i%s%d.nii.g\
-    -i $INPUT_DIR/$Surname_$Name$i/ \
-    -o OUTPUT_DIR
+    do    
+	TEMP_FOLDER=${INPUT_DIR}/"${Surname,,}_${Name,,}${i}/"
+	echo " $i-sequences from $TEMP_FOLDER"
+	
+	if [[ ! -d ${TEMP_FOLDER} ]]; then
+		echo
+		echo " ... folder ${TEMP_FOLDER} inexistent. Continuing!"
+		echo
+	
+	else
+		echo
+		echo " ... extracting sequences ..."
+		echo
+
+		${PATH2DCM2NIIX}/dcm2niix \
+		-b y \
+		-ba y \
+		-f ${pseud}_$i_%d.nii.gz\
+		-o ${OUTPUT_DIR} \
+		${TEMP_FOLDER}
+
+	fi
   done
-done < $INPUT
+echo "--------------------------------------------------------------------------------------"
+
+done < $METADATA
 IFS=$OLDIFS
+
+
+# A part is needed where only sequences of interest are maintained 
+
+# Defacing with python package should be included
